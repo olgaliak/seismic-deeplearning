71fb2b28988d99135dac1887d8c336af48c8bf7a
Anna Zietlow <anna.zietlow@microsoft.com>
2020-02-07 01:27:59 -0500

    add azureml requirements.txt for dutchf3 local patch and update pipeline config

--- interpretation/deepseismic_interpretation/azureml_pipelines/dev/kickoff_train_pipeline.py
+++ interpretation/deepseismic_interpretation/azureml_pipelines/dev/kickoff_train_pipeline.py
@@ -1,8 +1,8 @@
 """
 Create pipeline and kickoff run
 """
-from src.azml.train_pipeline.train_pipeline import TrainPipeline
+from deepseismic_interpretation.azureml_pipelines.train_pipeline import TrainPipeline
 
-orchestrator = TrainPipeline("train/pipeline_config.json")
+orchestrator = TrainPipeline("interpretation/deepseismic_interpretation/azureml_pipelines/pipeline_config.json")
 orchestrator.construct_pipeline()
 run = orchestrator.run_pipeline(experiment_name="DEV-train-pipeline")
